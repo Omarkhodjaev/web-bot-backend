325acb7d84b13923bd2d7e0ae23e7848071744b0
Umar  Khodjaev <125152511+Omarkhodjaev@users.noreply.github.com>
2024-06-10 16:09:07 +0500

    api

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const token = "7356982966:AAFDedgzE5MsKCyQesxQVCF49WQWfXxrC4g";
 const bot = new TelegramBot(token, { polling: true });
 
 const bootstrap = () => {
+  bot.setMyCommands([
+    { command: "/start", description: "Botni ishga tushirish" },
+    { command: "/courses", description: "Kurslar ro'yhatini ko'rish" },
+  ]);
+
   bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
@@ -11,7 +16,7 @@ const bootstrap = () => {
     if (text === "/start") {
       await bot.sendMessage(
         chatId,
-        "Platformada mavjud kurslarni sotib olishingiz mumkin",
+        `Assalomu Alaykum sizni ko'rib turganimizdan hursandmiz janob ${msg.chat.first_name}, biz sizga sifatli kurslarni tavsiya qilamiz`,
         {
           reply_markup: {
             keyboard: [
@@ -27,6 +32,55 @@ const bootstrap = () => {
           },
         }
       );
+    }
+
+    if (text === "/courses") {
+      await bot.sendMessage(
+        chatId,
+        "Platformada mavjud kurslarni sotib olishingiz mumkin",
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Kurslarni ko'rish",
+                  web_app: {
+                    url: "https://web-bot-client.vercel.app",
+                  },
+                },
+              ],
+            ],
+          },
+        }
+      );
+    }
+
+    if (msg.web_app_data?.data) {
+      try {
+        const data = JSON.parse(msg.web_app_data?.data);
+
+        await bot.sendMessage(chatId, "Sotib olingan kurslar: ");
+
+        for (const item of data) {
+          await bot.sendPhoto(chatId, item.Image, {
+            caption: `*Kurs nomi:* ${item.title}\n*Kurs soni:* ${item.quantity}x\n*Kurs narxi:* \$${item.price}`,
+            parse_mode: "Markdown",
+          });
+        }
+
+        await bot.sendMessage(
+          chatId,
+          `*💲Umumiy narx: *  ${data
+            .reduce((a, c) => a + c.price * c.quantity, 0)
+            .toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}`,
+          { parse_mode: "Markdown" }
+        );
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 };
